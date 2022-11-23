612fe4b564788c23be1fee4b95fe2eb9dc922890
Alexey Elkin <ae339@cornell.edu>
2022-11-23 00:23:37 -0500

    Added seekbar with tracking and controls

--- app/src/main/java/com/example/hm4_test/SongDetails.kt
+++ app/src/main/java/com/example/hm4_test/SongDetails.kt
@@ -1,14 +1,14 @@
 package com.example.hm4_test
 
-import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class SongDetails : AppCompatActivity() {
     lateinit var music : MediaPlayer
@@ -32,7 +32,6 @@ class SongDetails : AppCompatActivity() {
         val cover : ImageView = findViewById(R.id.detailedCover)
         cover.setImageResource(MainActivity.songList[position].cover)
 
-
         // Code responsible for playing the music file itself
         val playButton = findViewById<ImageButton>(R.id.playButton)
         val pauseButton = findViewById<ImageButton>(R.id.pauseButton)
@@ -44,49 +43,61 @@ class SongDetails : AppCompatActivity() {
         music = MediaPlayer()
         music.setAudioStreamType(AudioManager.STREAM_MUSIC)
 
-//        music.setDataSource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
-//        music.prepare()
+        // The seekbar representing the song progress
+        val seekBar : SeekBar = findViewById(R.id.songProgress)
 
-        var loaded : Boolean = false
+        // Making the progress bar update every second
+        var handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                try {
+                    seekBar.progress = music.currentPosition
+                    handler.postDelayed(this, 1000)
+                } catch (e: Exception) {
+                    seekBar.progress = 0
+                }
+            }
+        }, 0)
 
+
+        var loaded = false
 
         playButton.setOnClickListener{
             if (!loaded) {
                 music.setDataSource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
                 music.prepare()
                 loaded = true
+                seekBar.max = music.duration
             }
             music.start()
         }
         pauseButton.setOnClickListener {
+            Log.d("music", music.currentPosition.toString())
+            Log.d("music", music.duration.toString())
             music.pause()
         }
         stopButton.setOnClickListener {
             music.pause()
             music.seekTo(0)
+            // stop, reset, release, =null
         }
 
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Allows the user to change the seekbar value dynamically
+                if (fromUser) {
+                    Log.d("music", "stfu")
+                    music.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
 
 
-//        // Initializing the Input Fields
-//        val editName : EditText = findViewById(R.id.editSongName)
-//        val editAlbum : EditText = findViewById(R.id.editAlbumName)
-//        val editArtist : EditText = findViewById(R.id.editArtistName)
-//
-//        // Setting the placeholder text values for the EditTexts
-//        editName.setText(name.text)
-//        editAlbum.setText(artist.text)
-//        editArtist.setText(album.text)
-//
-//        val saveButton : Button = findViewById(R.id.saveButton)
-//        saveButton.setOnClickListener {
-//            MainActivity.songList[position].name = editName.text.toString()
-//            MainActivity.songList[position].artist = editArtist.text.toString()
-//            MainActivity.songList[position].album = editAlbum.text.toString()
-//
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//        }
+
 
         // TODO: User should be redirected back to the Main Activity
     }
